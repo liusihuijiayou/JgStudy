50af633fcb87c6d0ace237aed1a7ca870ac4e4c6
liusihuillll <liush@citydo.com.cn>
2020-06-08 15:46:47 +0800

    星级评价

--- app/src/main/java/com/lsh/jgstudy/MainActivity.java
+++ app/src/main/java/com/lsh/jgstudy/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import com.lsh.jgstudy.activity.OrderActivity;
 import com.lsh.jgstudy.aop.DBOperation;
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity implements DBOperation {
 
     private DBOperation db;
     private final static String TAG="netease >>>";
+    private RatingBar ratingBar;//星级评分条
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +51,24 @@ public class MainActivity extends AppCompatActivity implements DBOperation {
 
         //运行时动态代理 做了一个切面
         db= (DBOperation) Proxy.newProxyInstance(DBOperation.class.getClassLoader(),new Class[]{DBOperation.class},new DBHander(this));
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar1);
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /**
+                 * getRating():用于获取等级，表示选中的几颗星
+                 * getStepSize():用语获取每次至少要改变多少个星级
+                 * getProgress():用语获取进度，获取到的进度值为getRating()方法返回值与getStepSize()方法返回值之积
+                 */
+                int result = ratingBar.getProgress();
+                float rating = ratingBar.getRating();
+                float step = ratingBar.getStepSize();
+                Log.e("星级评分条","step="+step+"result="+result+"rating="+rating);
+                Toast.makeText(MainActivity.this,"你得到了"+rating+"颗星",Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 
